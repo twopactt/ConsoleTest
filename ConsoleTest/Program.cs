bd0f9f76df9357ca76f37f0faa7bdce7014f1223
twopact <artemov9321@mail.ru>
2024-11-13 19:22:21 +0300

    Изменил до 123

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleTest
         static void Main(string[] args)
         {
             var fileName = "MLstartConfig.json";
-            var content = @"{ ""Test"": ""1234435667""}";
+            var content = @"{ ""Test"": ""123""}";
             File.WriteAllText(fileName, content);
             IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName).Build();
             string testConfig = configuration["Test"];
