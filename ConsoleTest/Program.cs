787583db6451db4bd3ef15d514c2df41cdf53eef
twopact <artemov9321@mail.ru>
2024-11-13 19:13:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "MLstartConfig.json";
+            var content = @"{ ""Test"": ""1234""}";
+            File.WriteAllText(fileName, content);
+            IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName).Build();
+            string testConfig = configuration["Test"];
+            Console.WriteLine(testConfig);
+        }
+    }
+}
